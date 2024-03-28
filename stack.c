ebd21bf4dda0d483e60d05e68bce7c6af6578db4
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 00:41:25 +0300

    program to display the top stack

--- stack.c
+++ stack.c
@@ -1,10 +1,12 @@
 #include "monty.h"
+
 /**
  * f_stack - program to display the top stack
  * @head: the stack head
  * @counter: number of lines inputed
  * Return: sucess NULL, -1 error
 */
+
 void f_stack(stack_t **head, unsigned int counter)
 {
 	(void)head;
