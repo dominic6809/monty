eac2a6bdb418db672e450d586e2316bf1e28f0da
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 02:29:29 +0300

    function that rotates the stack to the bottom.

--- rotate1.c
+++ rotate1.c
@@ -7,21 +7,21 @@
  */
 void rotr(stack_t **head, unsigned int num_digit)
 {
-    stack_t *current;
+	stack_t *current;
 
-    (void)num_digit;
+	(void)num_digit;
 
-    if (*head && (*head)->next)
-    {
-        current = *head;
+	if (*head && (*head)->next)
+	{
+		current = *head;
 
-        while (current->next)
-            current = current->next;
+		while (current->next)
+			current = current->next;
 
-        current->prev->next = NULL;
-        current->prev = NULL;
-        current->next = *head;
-        (*head)->prev = current;
-        *head = current;
-    }
+		current->prev->next = NULL;
+		current->prev = NULL;
+		current->next = *head;
+		(*head)->prev = current;
+		*head = current;
+	}
 }
