93252ca8017b4d918b2b4e1ad3f92c0589da5001
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-28 23:04:27 +0300

    mul.c

---
+++ mul.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+/**
+ * f_mul - multiplies the top two elements of stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *t;
+	int len = 0, aux;
+
+	t = *head;
+	while (t)
+	{
+		t = t->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	t = *head;
+	aux = t->next->n * t->n;
+	t->next->n = aux;
+	*head = t->next;
+	free(t);
+}
