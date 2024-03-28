bb43c1ab5dc4d1c2385941d69103364d794c0ab2
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 00:39:46 +0300

    program to perform subtraction operation

--- sub.c
+++ sub.c
@@ -15,7 +15,7 @@ void f_sub(stack_t **head, unsigned int counter)
 		aux = aux->next;
 	if (nodes < 2)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", counter);
+		fprintf(stderr, "L%d: cannot subtract, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
