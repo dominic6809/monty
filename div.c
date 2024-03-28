f42316be28ee584580d64aa83a283567b12ba8b3
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 00:35:20 +0300

    function that divides the top two elements of the stack

--- div.c
+++ div.c
@@ -1,31 +1,35 @@
 #include "monty.h"
+
 /**
- * f_div - divides the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: null
+ * f_div - function that divides the top two elements of the stack.
+ * @head: head of the stack
+ *
+ * @counter: number of lines
+ *
+ * Return: NULL, upon successful execution
 */
+
 void f_div(stack_t **head, unsigned int counter)
 {
-	stack_t *t;
-	int len = 0, aux;
+	stack_t *u;
+	int val = 0, aux;
 
-	t = *head;
+	u = *head;
 	while (t)
 	{
-		t = t->next;
-		len++;
+		u = u->next;
+		val++;
 	}
-	if (len < 2)
+	if (val < 2)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fprintf(stderr, "L%d: cannot div, stack too short\n", counter);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	t = *head;
-	if (t->n == 0)
+	u = *head;
+	if (u->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(bus.file);
@@ -33,8 +37,8 @@ void f_div(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = t->next->n / t->n;
-	t->next->n = aux;
-	*head = t->next;
-	free(t);
+	aux = u->next->n / u->n;
+	u->next->n = aux;
+	*head = u->next;
+	free(u);
 }
