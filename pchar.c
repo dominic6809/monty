bfdad5553e12a43bebf7ecaa62fa6c417490e383
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 01:28:47 +0300

    function that prints the char at the top of the stack

--- pchar.c
+++ pchar.c
@@ -5,19 +5,19 @@
  * @stack: Double pointer to the beginning of the stack
  * @line_number: The current line number in the Monty file
  */
-void pchar_op(stack_t **stack, unsigned int line_number)
+void pchar(stack_t **head, unsigned int num_digit)
 {
-    if (!stack || !(*stack))
+    if (!stack || !(*head))
     {
         fprintf(stderr, "L%d: can't pchar, stack empty\n", num_digit);
         exit(EXIT_FAILURE);
     }
 
-    if ((*stack)->n < 0 || (*stack)->n > 127)
+    if ((*head)->n < 0 || (*head)->n > 127)
     {
         fprintf(stderr, "L%d: cannot pchar, value out of range\n", num_digit);
         exit(EXIT_FAILURE);
     }
 
-    printf("%c\n", (*stack)->n);
+    printf("%c\n", (*head)->n);
 }
