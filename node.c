44761a9c0ff77f5d8268b4dc7db75b67d27f6816
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 00:16:04 +0300

    function that adds node to the head of stack

--- node.c
+++ node.c
@@ -1,23 +1,25 @@
 #include "monty.h"
+
 /**
- * addnode - add node to the head of stack
- * @head: head of the stack
- * @n: new_value
- * Return: null
+ * addnode - function that adds node to the head of stack
+ * @head: head stack
+ * @n: new value obtained
+ * Return: NULL, success
 */
+
 void addnode(stack_t **head, int n)
 {
-	stack_t *new_node, *aux;
+	stack_t *new_n, *aux;
 
 	aux = *head;
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	new_n = malloc(sizeof(stack_t));
+	if (new_n == NULL)
 	{ printf("Error\n");
 		exit(0); }
 	if (aux)
-		aux->prev = new_node;
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	*head = new_node;
+		aux->prev = new_n;
+	new_n->n = n;
+	new_n->next = *head;
+	new_n->prev = NULL;
+	*head = new_n;
 }
