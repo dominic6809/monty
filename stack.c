1b8046ae8a713add1160e94af874f518bf8fb3b3
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-28 23:08:42 +0300

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: null
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
