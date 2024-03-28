a2e0a7ff1174235e1b887aa605dcc8c85db9829c
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-28 23:42:43 +0300

    function that adds the top two elements of the stack

--- swap.c
+++ swap.c
@@ -1,22 +1,22 @@
 #include "monty.h"
 /**
- * f_swap - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: null
+ * f_swap - function that adds the top two elements of the stack.
+ * @head: stack head to point to the next
+ * @counter: number of lines
+ * Return: null success
 */
 void f_swap(stack_t **head, unsigned int counter)
 {
-	stack_t *t;
-	int len = 0, aux;
+	stack_t *u;
+	int val = 0, aux;
 
-	t = *head;
-	while (t)
+	u = *head;
+	while (u)
 	{
-		t = t->next;
-		len++;
+		u = u->next;
+		val++;
 	}
-	if (len < 2)
+	if (val < 2)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
 		fclose(bus.file);
@@ -24,8 +24,8 @@ void f_swap(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	t = *head;
-	aux = t->n;
-	t->n = t->next->n;
-	t->next->n = aux;
+	u = *head;
+	aux = u->n;
+	u->n = u->next->n;
+	u->next->n = aux;
 }
