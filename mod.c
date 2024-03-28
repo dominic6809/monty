c49fd70262b714260ba94e9bea520669969b33c0
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 01:46:57 +0300

    Computes the rest of the division of the second top element

--- mod.c
+++ mod.c
@@ -10,23 +10,29 @@ void mod(stack_t **head, unsigned int num_digit)
 {
     stack_t *top, *second_top;
 
-    if (!stack || !(*stack) || !((*stack)->next))
+    if (!stack || !(*head) || !((*head)->next))
     {
         fprintf(stderr, "L%d: cannot mod, stack too short\n", num_digit);
+        fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
         exit(EXIT_FAILURE);
     }
 
-    top = *stack;
+    top = *head;
     second_top = top->next;
 
     if (top->n == 0)
     {
         fprintf(stderr, "L%d: division by zero\n", num_digit);
+        fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
         exit(EXIT_FAILURE);
     }
 
     second_top->n %= top->n;
-    *stack = second_top;
-    (*stack)->prev = NULL;
+    *head = second_top;
+    (*head)->prev = NULL;
     free(top);
 }
