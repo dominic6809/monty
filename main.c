32ddb390ced481bdfff34bcaeac5b7126257775f
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-28 23:03:21 +0300

    main.c

--- main.c
+++ main.c
@@ -1,44 +1,45 @@
 #include "monty.h"
-#define _GNU_SOURCE
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
- * main - monty code interpreter
- * @argc: number of arguments
- * @argv: monty file location
- * Return: 0 on success, otherwise error
- */
+* main - entry-point
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	char *line_content;
 	FILE *monty_file;
-	size_t buffer_size = 0;
-	ssize_t read_line = 1;
+	size_t line_size = 0;
+	ssize_t read_result = 1;
 	stack_t *stack = NULL;
-	unsigned int line_counter = 0;
+	unsigned int counter = 0;
 
 	if (argc != 2)
-	process_usage_err();
-
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 	monty_file = fopen(argv[1], "r");
 	bus.file = monty_file;
 	if (!monty_file)
-	process_f_open_err(argv[1]);
-
-	while (read_line > 0)
 	{
-	line_content = NULL;
-	read_line = getline(&line_content, &buffer_size, monty_file);
-	bus.content = line_content;
-	line_counter++;
-	if (read_line > 0)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_result > 0)
 	{
-	execute(line_content, &stack, line_counter, monty_file);
+		line_content = NULL;
+		read_result = getline(&line_content, &line_size, monty_file);
+		bus.content = line_content;
+		counter++;
+		if (read_result > 0)
+		{
+			execute(line_content, &stack, counter, monty_file);
+		}
+		free(line_content);
 	}
-	free(line_content);
-	}
-	process_free_stack(stack);
+	free_stack(stack);
 	fclose(monty_file);
-	return (0);
+return (0);
 }
