040d9cc53bca27970131a40ccc58b095889e2b4f
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-29 02:32:20 +0300

    function that prints the char at the top of the stack

--- pchar.c
+++ pchar.c
@@ -7,23 +7,23 @@
  */
 void pchar(stack_t **head, unsigned int num_digit)
 {
-    if (!stack || !(*head))
-    {
-        fprintf(stderr, "L%d: cannot pchar, stack empty\n", num_digit);
-	fclose(bus.file);
-	free(bus.content);
-	free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	if (!stack || !(*head))
+	{
+		fprintf(stderr, "L%d: cannot pchar, stack empty\n", num_digit);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    if ((*head)->n < 0 || (*head)->n > 127)
-    {
-        fprintf(stderr, "L%d: cannot pchar, value out of range\n", num_digit);
-        fclose(bus.file);
-	free(bus.content);
-	free_stack(*head);
-        exit(EXIT_FAILURE);
-    }
+	if ((*head)->n < 0 || (*head)->n > 127)
+	{
+		fprintf(stderr, "L%d: cannot pchar, value out of range\n", num_digit);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
 
-    printf("%c\n", (*head)->n);
+	printf("%c\n", (*head)->n);
 }
